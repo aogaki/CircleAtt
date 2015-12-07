a2c8c63b89edff9e142c427bb2dfb38350fee9fa
aogaki <sohichiroh.aogaki@eli-np.ro>
2015-12-07 11:25:32 +0200

    test

--- parallel.sh
+++ parallel.sh
@@ -4,6 +4,7 @@
 kEvents="1000000"
 echo "/run/beamOn $kEvents" > test.mac
 
-cat threads | parallel -j 8 ./wrapper.sh
+cat threads | parallel -j 100% ./wrapper.sh
 outName="result"`cat simInfo`".root"
 hadd -f $outName tmp*.root
+rm -f tmp*.root
