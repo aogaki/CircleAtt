0b0bcdd2987b32d7fd83e118f0cc9c5b859c7e28
aogaki <sohichiroh.aogaki@eli-np.ro>
2015-12-07 16:14:20 +0200

    for grid increasing the event number

--- parallel.sh
+++ parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#kEvents="100000000"
-kEvents="1000000"
+kEvents="100000000"
+#kEvents="1000000"
 echo "/run/beamOn $kEvents" > test.mac
 
 cat threads | parallel -j 100% ./wrapper.sh
